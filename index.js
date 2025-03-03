e331a3b0d88c3effcfe3d96667046008a0b5268e
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-04 01:32:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const con = require('./models/user');
 
 // Middleware setup
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
